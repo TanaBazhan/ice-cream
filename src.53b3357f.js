0a1412609f2e78490f8cddbcb3cd65506f26b1de
TanaBazhan <87903456+TanaBazhan@users.noreply.github.com>
2021-10-13 20:20:37 +0000

    Deploying to gh-pages from @ TanaBazhan/ice-cream@e00ac777b55341df200e25bea21e3acb19af9c0e 🚀

--- src.53b3357f.js
+++ src.53b3357f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/logo.svg":[["logo.165aaa91.svg","pLZI"],"pLZI"],"./../images/about/secbg-mob2x.png":[["secbg-mob2x.405d0ac2.png","Im1C"],"Im1C"],"./../images/advantages/icon1.svg":[["icon1.ef9a5fc6.svg","nc2W"],"nc2W"],"./../images/advantages/icon2.svg":[["icon2.08ad7078.svg","Lcfl"],"Lcfl"],"./../images/advantages/icon3.svg":[["icon3.255fb4d1.svg","PUPX"],"PUPX"],"./../images/customer-reviews/commas.svg":[["commas.d20c615d.svg","IYB2"],"IYB2"],"./../images/customer-reviews/dots.svg":[["dots.ac49ee34.svg","VrgX"],"VrgX"],"./../images/contacts/background-img.png":[["background-img.ba2c8189.png","XARX"],"XARX"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-project/src.60ac06ae.js.map
+//# sourceMappingURL=/ice-cream/src.53b3357f.js.map
